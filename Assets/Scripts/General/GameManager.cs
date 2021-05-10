a5c4f7170be991ba7ba4341a3997d36fde696da5
daskatyusha <younver48@gmail.com>
2021-05-10 15:23:53 +0300

    +clean scene hierarchy +added: general settings script manager

---
+++ Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+    }
+}
